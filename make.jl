6cff077e0334837b3f76c586c396eb1c2b457557
Josselin Morvan <morvan.josselin@gmail.com>
2026-06-09 15:59:31 +0200

    first commit

---
+++ make.jl
@@ -0,0 +1,18 @@
+using Documenter
+
+makedocs(;
+  format = Documenter.HTML(;
+    prettyurls = get(ENV, "CI", nothing) == "true"
+  ),
+  authors = "E. Château-Dutier <emmanuel.chateau.dutier@umontreal.ca>, J. Dehut <julien.dehut@univ-rouen.fr>, J. Morvan <josselin.morvan@univ-rouen.fr>",
+  pages = [
+    "JuliaDH" => "index.md"
+  ],
+  sitename = "JuliaDH",
+  #repo = "https://github.com/juliadh/juliadh"
+  #remotes=nothing
+)
+
+deploydocs(
+  repo = "https://github.com/juliadh/juliadh"
+)
